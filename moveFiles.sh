e59acc7a8eac6dac45e1978ffd5e86f147334260
c0d3m4st4 <c0d3m4st4@users.noreply.github.com>
2018-03-06 03:41:30 +0100

    updated "install" script

--- moveFiles.sh
+++ moveFiles.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-echo "Moving files..."
-mv ./idc/*.* ./ps4/exploits/idc/
-mv ./lightningmods/Webpage\ Exploit/*.* ./ps4/exploits/lightningmods/
-mv ./specter/*.* ./ps4/exploits/specter/
-mv ./xvortex-dumper/exploit/*.* ./ps4/exploits/xvortex-dumper/
-mv ./xvortex-ftp/exploit/*.* ./ps4/exploits/xvortex-ftp/
-mv ./xvortex-hen/exploit/*.* ./ps4/exploits/xvortex-hen/
+echo "Deleting old files..."
+rm -r /var/www/html/*
+
+echo "Moving exploit files..."
+mv ./lightningmods/Webpage\ Exploit/*.* ./ps4/exploits/405/lightningmods/
+mv ./xvortex-dumper/exploit/*.* ./ps4/exploits/405/xvortex-dumper/
+mv ./xvortex-ftp/exploit/*.* ./ps4/exploits/405/xvortex-ftp/
+mv ./xvortex-hen/exploit/*.* ./ps4/exploits/405/xvortex-hen/
+
+mv ./xvortex-dumper-455/exploit/*.* ./ps4/exploits/455/xvortex-dumper/
+mv ./xvortex-ftp-455/exploit/*.* ./ps4/exploits/455/xvortex-ftp/
+mv ./xvortex-hen-455/exploit/*.* ./ps4/exploits/455/xvortex-hen/
 
 echo "Cleaning up..."
-rm -rf ./idc/
 rm -rf ./lightningmods/
-rm -rf ./specter/
 rm -rf ./xvortex-dumper/
 rm -rf ./xvortex-ftp/
 rm -rf ./xvortex-hen/
+rm -rf ./xvortex-dumper-455/
+rm -rf ./xvortex-ftp-455/
+rm -rf ./xvortex-hen-455/
 
 echo "Self deleting..."
 rm moveFiles.sh
