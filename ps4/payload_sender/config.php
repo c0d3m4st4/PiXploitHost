56ac4e2ce643cb99af945eb905f179e6e73a9f45
c0d3m4st4 <c0d3m4st4@users.noreply.github.com>
2018-03-14 02:17:52 +0100

    Add PHP bin payload sender

---
+++ ps4/payload_sender/config.php
@@ -0,0 +1,12 @@
+<?php 
+    
+	// bin payload directory
+	define("PAYLOADS_DIR", "./bin_payloads/");
+	
+	// listening server IP address
+	define("SERVER_IP", "192.168.3.19");
+	
+	// listening server port
+	define("SERVER_PORT", "9020");
+
+?>
