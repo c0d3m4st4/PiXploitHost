9c371a77c5f19ce4c24a1c7f4194def7d211af8a
c0d3m4st4 <c0d3m4st4@users.noreply.github.com>
2018-03-14 02:35:03 +0100

    Added specter submodule

--- moveFiles.sh
+++ moveFiles.sh
@@ -8,19 +8,23 @@ mv ./lightningmods/Webpage\ Exploit/*.* ./ps4/exploits/405/lightningmods/
 mv ./xvortex-dumper/exploit/*.* ./ps4/exploits/405/xvortex-dumper/
 mv ./xvortex-ftp/exploit/*.* ./ps4/exploits/405/xvortex-ftp/
 mv ./xvortex-hen/exploit/*.* ./ps4/exploits/405/xvortex-hen/
+mv ./specter/exploit/*.* ./ps4/exploits/455/specter/
 
 mv ./xvortex-dumper-455/exploit/*.* ./ps4/exploits/455/xvortex-dumper/
 mv ./xvortex-ftp-455/exploit/*.* ./ps4/exploits/455/xvortex-ftp/
 mv ./xvortex-hen-455/exploit/*.* ./ps4/exploits/455/xvortex-hen/
+mv ./specter-455/exploit/*.* ./ps4/exploits/455/specter/
 
 echo "Cleaning up..."
 rm -rf ./lightningmods/
 rm -rf ./xvortex-dumper/
 rm -rf ./xvortex-ftp/
 rm -rf ./xvortex-hen/
+rm -rf ./specter/
 rm -rf ./xvortex-dumper-455/
 rm -rf ./xvortex-ftp-455/
 rm -rf ./xvortex-hen-455/
+rm -rf ./specter-455/
 
 echo "Self deleting..."
 rm moveFiles.sh
